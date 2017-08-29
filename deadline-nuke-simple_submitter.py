183eb24c588129c161bc16edb09947712861a51c
NodesLayers <ricardo@nodeslayers.com>
2017-08-29 18:50:05 +0100

    Reworked part of the script

--- deadline-nuke-simple_submitter.py
+++ deadline-nuke-simple_submitter.py
@@ -1,5 +1,5 @@
 ## Settings
-title = "Deadline: Simple Farm Submitter"
+title = "Deadline - Simple Farm Submitter"
 my_version = "v0.1"
 deadline_command_path = "C:\\Program Files\\Thinkbox\\Deadline8\\bin\\deadlinecommand.exe"
 nuke_script_name = os.path.basename(nuke.root().name())
@@ -12,6 +12,11 @@ last_frame = int(nuke.root()["last_frame"].getValue())
 
 
 
+## START
+print ""
+print title+" "+my_version
+
+
 ## PANEL
 p = nuke.Panel('Simple Deadline Submitter '+my_version)    
 p.addSingleLineInput('Framerange:', str(first_frame)+"-"+str(last_frame))
@@ -22,18 +27,23 @@ p.addBooleanCheckBox('Do not re-arrange frames', False)
 ret = p.show()
 
 
+
 ## PROCESSING
-
-
 
 ## Framerange
 framerange = p.value("Framerange:")
 framerange = framerange.split("-")
 first_frame = int(framerange[0])
 last_frame = int(framerange[1])
-
-
-
 middle_frame = ((last_frame-first_frame)/2+1)+first_frame
-#print middle_frame
+print "Framerange: "+str(first_frame)+"-"+str(last_frame)
 
+
+concurrent_tasks = p.value("Complexity:")
+if concurrent_tasks == "Simple":
+    concurrent_tasks = 3
+if concurrent_tasks == "Medium":
+    concurrent_tasks = 2
+if concurrent_tasks == "Heavy":
+    concurrent_tasks = 1
+print "Concurrent tasks: "+str(concurrent_tasks)
