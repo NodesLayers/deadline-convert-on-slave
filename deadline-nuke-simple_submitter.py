17de8e96f5bb9b0df6596c97d294173a19769c75
NodesLayers <ricardo@nodeslayers.com>
2017-08-29 18:32:05 +0100

    Coding Sumitter

--- deadline-nuke-simple_submitter.py
+++ deadline-nuke-simple_submitter.py
@@ -1,19 +1,16 @@
 ## Settings
 title = "Deadline: Simple Farm Submitter"
-deadline_command_path = ""C:\\Program Files\\Thinkbox\\Deadline8\\bin\\deadlinecommand.exe""
+my_version = "v0.1"
+deadline_command_path = "C:\\Program Files\\Thinkbox\\Deadline8\\bin\\deadlinecommand.exe"
 nuke_script_name = os.path.basename(nuke.root().name())
+prio_message = "Request Priority"
 
 
-
-
-first_frame = int("1")
-last_frame = int("100")
-middle_frame = (last_frame-first_frame)/2+1
-print middle_frame
-
-
-
-
+## Parameters
+first_frame = int(nuke.root()["first_frame"].getValue())
+last_frame = int(nuke.root()["last_frame"].getValue())
+middle_frame = ((last_frame-first_frame)/2+1)+first_frame
+#print middle_frame
 
 
 
@@ -21,12 +18,12 @@ print middle_frame
 
 
 ## PANEL
-p = nuke.Panel('MAELSTROM WRITER '+myVersion)    
-p.addEnumerationPulldown('Show:', "DNG02_MAE")
-p.addEnumerationPulldown('Context:', sequence+"_"+shot)
-p.addEnumerationPulldown('Render type:', 'comp precomp element')
-p.addEnumerationPulldown('Format:', 'movie exr-sequence')
-p.addSingleLineInput('(optional) Identifier:', '')
+p = nuke.Panel('Simple Deadline Submitter '+my_version)    
+p.addSingleLineInput('Framerange:', str(first_frame)+"-"+str(last_frame))
+p.addEnumerationPulldown('Complexity:', "Simple Medium Heavy")
+p.addBooleanCheckBox('Render slate', True)
+p.addBooleanCheckBox('Ask for priority', False)
+p.addBooleanCheckBox('Do not re-arrange frames', False)
 
 
 ret = p.show()
@@ -34,7 +31,4 @@ ret = p.show()
 
 
 
-## Set User Variables
-render_type = p.value("Render type:")
-format = p.value("Format:")
-ident = p.value("(optional) Identifier:")
+
